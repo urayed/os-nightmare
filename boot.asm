5c38cb6cae984cdb851cba01bceb96e8b1c1b87e
wibwib <ray@dokholyan.org>
2025-07-16 23:56:31 -0400

    simple writing

--- boot.asm
+++ boot.asm
@@ -1,5 +1,31 @@
-[org 0x7c00]           
-jmp $                  
+[org 0x7c00]      
+
+
+
+
+
+
+getChar:
+    ;db 0
+    mov ah, 0
+    int 0x16
+    ;mov [bx], al
+    jmp showChar
+
+showChar:
+    
+    mov ah, 0x0e
+    
+    cmp al, 13
+    je exit
+    int 0x10
+    jmp getChar
+
+
+
+exit:
+    jmp $
+
 
 times 510-($-$$) db 0  
 db 0x55, 0xaa          
