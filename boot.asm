4da216f1bed39f6184fcf32175237acf25cfea40
wibwib <ray@dokholyan.org>
2025-07-17 13:14:39 -0400

    now echoes strings!!! i hate assembly so much

--- boot.asm
+++ boot.asm
@@ -1,26 +1,79 @@
 [org 0x7c00]      
 
+newLine:
+    mov ah, 0x0e
+    mov al, 13      
+    int 0x10
+    mov al, 10      
+    int 0x10
+    mov al, '>'
+    int 0x10
+    jmp clearBuffer
 
 
 
+buffer:
 
+    times 10 db 0
+    mov bx, buffer
+    jmp getChar
+
+clearBuffer: 
+    mov cx, 10
+    mov di, buffer
+    jmp clearLoop
+
+clearLoop: 
+    mov byte [di], 0
+    inc di
+    loop clearLoop
+    jmp buffer
 
 getChar:
-    ;db 0
     mov ah, 0
     int 0x16
-    ;mov [bx], al
+    
     jmp showChar
 
 showChar:
     
     mov ah, 0x0e
-    
+
     cmp al, 13
-    je exit
+    je preparePrint
+
     int 0x10
+
+    cmp bx, buffer + 9
+    jae preparePrint
+
+    jmp addCharToBuffer
+    
+
+addCharToBuffer:
+    mov [bx], al
+    inc bx
     jmp getChar
 
+preparePrint:
+    mov ah, 0x0e
+    mov bx, buffer
+    mov al, 13
+    int 0x10
+    mov al, 10
+    int 0x10
+    jmp print
+
+print:
+    
+
+    cmp bx, buffer + 9
+    jae newLine
+
+    mov al, [bx]
+    int 0x10
+    inc bx
+    jmp print
 
 
 exit:
